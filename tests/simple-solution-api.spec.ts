72f5171ea2914012691b278524b5a977655273b7
Karina Huhhareva <karina.huhhareva@gmail.com>
2025-12-28 16:57:28 +0200

    HW9

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -2,18 +2,15 @@ import { expect, test } from '@playwright/test'
 
 import { StatusCodes } from 'http-status-codes'
 
+const BASE_URL = 'https://backend.tallinn-learning.ee/test-orders';
+
 test('get order with correct id should receive code 200', async ({ request }) => {
-  // Build and send a GET request to the server
-  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/1')
-  // Log the response status, body and headers
-  console.log('response body:', await response.json())
-  console.log('response headers:', response.headers())
-  // Check if the response status is 200
+  const response = await request.get(`${BASE_URL}/1`)
+
   expect(response.status()).toBe(200)
 })
 
-test('post order with correct data should receive code 201', async ({ request }) => {
-  // prepare request body
+test('post order with correct data should receive code 200', async ({ request }) => {
   const requestBody = {
     status: 'OPEN',
     courierId: 0,
@@ -22,12 +19,88 @@ test('post order with correct data should receive code 201', async ({ request })
     comment: 'string',
     id: 0,
   }
-  // Send a POST request to the server
-  const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
+
+  const response = await request.post(BASE_URL, {
     data: requestBody,
   })
-  // Log the response status and body
-  console.log('response status:', response.status())
-  console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.OK)
+})
+
+test('get order with incorrect data should receive code 400', async ({ request }) => {
+  const [response0, response11, responseNull, responseTest] = await Promise.all([
+    request.get(`${BASE_URL}/0`),
+    request.get(`${BASE_URL}/11`),
+    request.get(`${BASE_URL}/null`),
+    request.get(`${BASE_URL}/test`)
+  ]);
+
+  expect(response0.status()).toBe(StatusCodes.BAD_REQUEST)
+  expect(response11.status()).toBe(StatusCodes.BAD_REQUEST)
+  expect(responseNull.status()).toBe(StatusCodes.BAD_REQUEST)
+  expect(responseTest.status()).toBe(StatusCodes.BAD_REQUEST)
+})
+
+test('del order with incorrect header should receive code 401', async ({ request }) => {
+  const response = await request.delete(`${BASE_URL}/1`, {
+    headers: {
+      'api_key': ''
+    }
+  });
+  expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
+})
+
+test('del order with correct header should receive code 204', async ({ request }) => {
+  const response = await request.delete(`${BASE_URL}/1`, {
+    headers: {
+      'api_key': '1234567890123456'
+    }
+  });
+  expect(response.status()).toBe(StatusCodes.NO_CONTENT)
+})
+
+test('delete order with incorrect id should receive code 400', async ({ request }) => {
+  const response = await request.delete(`${BASE_URL}/999`, {
+    headers: {},
+    data: {
+      customerName: 'string',
+    }
+  });
+
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
+})
+
+test('update order with correct header should receive code 200', async ({ request }) => {
+  const response = await request.put(`${BASE_URL}/1`, {
+    headers: {
+      'api_key': '1234567890123456'
+    },
+    data: {
+      customerName: 'string',
+    }
+  });
+  expect(response.status()).toBe(StatusCodes.OK)
+})
+
+test('update order with incorrect header should receive code 401', async ({ request }) => {
+  const response = await request.put(`${BASE_URL}/1`, {
+    headers: {
+      'api_key': ''
+    },
+    data: {
+      customerName: 'string',
+    }
+  });
+  expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
+})
+
+test('update order with incorrect id should receive code 400', async ({ request }) => {
+  const response = await request.put(`${BASE_URL}/999`, {
+    headers: {
+      'api_key': '1234567890123456'
+    },
+    data: {
+      customerName: 'string'
+    }
+  });
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
